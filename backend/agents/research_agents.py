c48a01cb8ff5dc817366db44c8c87a000147f00a
anilgehlotn <anilgehlotn@gmail.com>
2026-07-22 16:00:48 +0530

    refactor: backend cleanup and gemini migration

--- backend/agents/research_agents.py
+++ backend/agents/research_agents.py
@@ -1,14 +1,14 @@
 from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from tools import web_search , scrape_url 
+from .tools import web_search , scrape_url
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#model setup 
-llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
+#model setup
+llm = ChatGoogleGenerativeAI(model = "gemini-2.0-flash",temperature=0)
 
 
 #1st agent 
